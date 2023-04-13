e9fc05c739fed0fe43e763889e743b5d8b2241c2
Sofia Ruban <sophiaruban249324@gmail.com>
2023-04-13 18:39:48 +0300

    Add new projects

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,33 +1,39 @@
-import logo from './logo.png';
-import './Header.css';
-import React from 'react';
+import logo from "./logo.png";
+import "./Header.css";
+import React from "react";
 
 class Header extends React.Component {
-    render(){
-        return (
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <nav id = "about"className="navbar">
-                <ul className= "nav-menu">
-                    <li className="nav-item">
-                        <a href="#navbar" className="nav-link">About me </a> 
-                    </li>
-                    <li className="nav-item">
-                        <a href="#skills" className="nav-link">My skills</a> 
-                    </li>
-                    <li className="nav-item">
-                        <a href="#projects" className="nav-link">Projects</a> 
-                    </li>
-                    <li className="nav-item">
-                        <a href="#contact" className="nav-link">Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        )
-    }
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <nav id="about" className="navbar">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <a href="#navbar" className="nav-link">
+                ABOUT ME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#skills" className="nav-link">
+                MY SKILLS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#projects" className="nav-link">
+                PROJECTS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#contacts" className="nav-link">
+                CONTACTS
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 }
-
-
 
 export default Header;
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Main.css";
 import logo_app from "./logo_app.png";
-import keyboard_app from "./keyboard_app.PNG";
-import weather_app from "./weather_app.PNG";
+import keyboard_app from "./keyboard_app.png";
+import todolist from "./todolist.png";
+import calendar_app from "./calendar_app.png";
+import rick_and_morty from "./rick-and-morty.png";
+import password_checker from "./password_checker.png";
+import job_app from "./job_app.png";
 import { BsFillTelephoneOutboundFill } from "react-icons/bs";
 import { FiMail } from "react-icons/fi";
 
@@ -19,28 +23,41 @@ class Main extends React.Component {
             <li>HTML</li>
             <li>CSS and CSS preprocessor &#40;Sass, SCSS&#41;</li>
             <li>JavaScript</li>
-            <li>Bundler &#40;Gulp, Webpack&#41;</li>
+            <li>TypeScript</li>
+            <li>React, Redux</li>
+            <li>Bootstrap, MUI, Tailwind</li>
             <li>Git</li>
             <li>Graphics Editor &#40;Photoshop, Figma&#41;</li>
-            <li>React</li>
           </ul>
         </div>
         <div id="projects" className="projects">
           <h2>Projects</h2>
           <div className="gallery">
             <a href="https://sofiiaruban.github.io/adaptive-project/">
-              <img src={logo_app}></img>
+              <img src={logo_app} alt="project"></img>
             </a>
             <a href="https://sofiiaruban.github.io/keyboard-app/">
-              <img src={keyboard_app}></img>
+              <img src={keyboard_app} alt="project"></img>
             </a>
-            <a href="https://sofiiaruban.github.io/weather-app/">
-              <img src={weather_app}></img>
+            <a href="https://sofiiaruban.github.io/todo-list/">
+              <img src={todolist} alt="project"></img>
+            </a>
+            <a href="https://sofiiaruban.github.io/meeting-booking-app/">
+              <img src={calendar_app} alt="project"></img>
+            </a>
+            <a href="https://sofiiaruban.github.io/rick-and-morty-app/">
+              <img src={rick_and_morty} alt="project"></img>
+            </a>
+            <a href="https://sofiiaruban.github.io/passwords-strength-checker">
+              <img src={password_checker} alt="project"></img>
+            </a>
+            <a href="https://sofiiaruban.github.io/job-app">
+              <img src={job_app} alt="project"></img>
             </a>
           </div>
         </div>
-        <div id="contact" className="contact">
-          <h2>Contact</h2>
+        <div id="contacts" className="contact">
+          <h2>Contacts</h2>
           <ul className="contact-list">
             <li>
               <BsFillTelephoneOutboundFill />
